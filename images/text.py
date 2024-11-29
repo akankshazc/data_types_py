08ce3055949eb798c3a8e30a3c8176768979884d
Akanksha <akankshazc23@icloud.com>
2024-11-29 23:49:48 +0530

    adding text to the image

---
+++ images/text.py
@@ -0,0 +1,16 @@
+# Import opencv
+import cv2
+
+# Read the image
+image = cv2.imread('van_gogh.jpg')
+
+# Copying the image as its an inplace operation
+output = image.copy()
+
+# adding text to the image
+text = cv2.putText(output, 'Van Gogh', (100, 100),
+                   cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 2)
+
+# Display the image
+cv2.imshow('Text', text)
+cv2.waitKey(2000)

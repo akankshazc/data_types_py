fdc074132c346dee7a6dfe4387cd586cdc0f9201
Akanksha <akankshazc23@icloud.com>
2024-11-29 23:04:43 +0530

    resizing the image

---
+++ images/resizing.py
@@ -0,0 +1,12 @@
+# Import opencv
+import cv2
+
+# Read the image
+image = cv2.imread('van_gogh.jpg')
+
+# Resize the image
+resized_image = cv2.resize(image, (200, 200))
+
+# Display the resized image
+cv2.imshow('Resized Image', resized_image)
+cv2.waitKey(2000)

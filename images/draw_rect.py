a1dd953422d6033217adc79f7444e102c546376d
Akanksha <akankshazc23@icloud.com>
2024-11-29 23:49:36 +0530

    drawing a rectangle over the image

---
+++ images/draw_rect.py
@@ -0,0 +1,15 @@
+# Import opencv
+import cv2
+
+# Read the image
+image = cv2.imread('van_gogh.jpg')
+
+# Copying the image as its an inplace operation
+output = image.copy()
+
+# Drawing a rectangle around the eyes in color green
+rectangle = cv2.rectangle(output, (250, 250), (1000, 700), (0, 255, 0), 2)
+
+# Display the image
+cv2.imshow('Rectangle', rectangle)
+cv2.waitKey(2000)

bb372ca55a26a8f5178e8b0819e29cf8746c2d72
Akanksha <akankshazc23@icloud.com>
2024-11-29 23:04:56 +0530

    resizing with proper aspect ratio maintained

---
+++ images/resize_aspect.py
@@ -0,0 +1,16 @@
+# Import opencv
+import cv2
+
+# Read the image
+image = cv2.imread('van_gogh.jpg')
+
+# Calculate the aspect ratio
+ratio = 200.0 / image.shape[1]
+
+# Creating a tuple with the new dimensions
+dimensions = (200, int(image.shape[0] * ratio))
+
+# Resize the image
+resized_image = cv2.resize(image, dimensions)
+cv2.imshow('Resized Image', resized_image)
+cv2.waitKey(2000)

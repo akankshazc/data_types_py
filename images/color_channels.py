8c6536cdbc0b0126df5874a43b5b5d7bdda67491
Akanksha <akankshazc23@icloud.com>
2024-11-30 15:39:20 +0530

    color channels

---
+++ images/color_channels.py
@@ -0,0 +1,20 @@
+import cv2
+
+# Reading the image
+image = cv2.imread('van_gogh.jpg')
+
+# Splitting the image into its color channels
+blue, green, red = cv2.split(image)
+
+# Display the image
+cv2.imshow('Blue Channel', blue)
+cv2.waitKey(2000)
+
+cv2.imshow('Green Channel', green)
+cv2.waitKey(2000)
+
+cv2.imshow('Red Channel', red)
+cv2.waitKey(2000)
+
+# Destroy all windows
+cv2.destroyAllWindows()
